a01dc6321a0c32826049b11874c9e58ea014e00b
Desenvolvimento15 <desenvolvimento15@dl.com.br>
2025-10-21 16:18:36 -0300

    vs01

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-$(".slick-track").slick({
-  arrows: false,
-  dots: false,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
 
 $(".slick-end").slick({
   lazyLoad: "ondemand",
   arrows: false,
   dots: false,
-  slidesToShow: 3,
+  slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
 });
+
